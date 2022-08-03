4d18168f4f7376cf5b77519e27fe53e8054dee37
Mario <mario.balukcic1@gmail.com>
2022-08-03 10:59:04 +0200

    SOBI-5087: Refactor, update packages, add swagger-ui-dist (#1)
    * SOBI-5087: Adding latest swagger-ui-dist
    
    * SOBI-5087: Refactoring
    
    * SOBI-5087: Removing loopback's eslint config
    
    * SOBI-5087: Updating mocha version

--- index.js
+++ index.js
@@ -5,10 +5,6 @@
 
 'use strict';
 
-const SG = require('strong-globalize');
-SG.SetRootDir(__dirname);
-const g = SG();
-
 /*!
  * Adds dynamically-updated docs as /explorer
  */
@@ -17,44 +13,56 @@ const path = require('path');
 const urlJoin = require('./lib/url-join');
 const _defaults = require('lodash').defaults;
 const createSwaggerObject = require('loopback-swagger').generateSwaggerSpec;
+const SWAGGER_UI_ROOT = require('swagger-ui-dist').absolutePath();
+const STATIC_ROOT = path.join(__dirname, 'public');
 
-module.exports = explorer;
-explorer.routes = routes;
 
 /**
- * Example usage:
+ * Setup Swagger documentation on the given express app.
  *
- * var explorer = require('loopback-component-explorer');
- * explorer(app, options);
+ * @param {Application} loopbackApplication The loopback application to
+ * document.
+ * @param {Application} swaggerApp Swagger application used for hosting
+ * swagger documentation.
+ * @param {Object} opts Options.
  */
+function mountSwagger(loopbackApplication, swaggerApp, opts) {
+  let swaggerObject = createSwaggerObject(loopbackApplication, opts);
 
-function explorer(loopbackApplication, options) {
-  options = _defaults({}, options, {mountPath: '/explorer'});
-  loopbackApplication.use(
-    options.mountPath,
-    routes(loopbackApplication, options)
-  );
-  loopbackApplication.set('loopback-component-explorer', options);
+  function rebuildSwaggerObject() {
+    swaggerObject = createSwaggerObject(loopbackApplication, opts);
+  }
+
+  // listening to modelRemoted event for updating the swaggerObject
+  // with the newly created model to appear in the Swagger UI.
+  loopbackApplication.on('modelRemoted', rebuildSwaggerObject);
+
+  loopbackApplication.on('modelDeleted', rebuildSwaggerObject);
+
+  // listening to started event for updating the swaggerObject
+  // when a call to app.models.[modelName].nestRemoting([modelName])
+  // to appear that method in the Swagger UI.
+  loopbackApplication.on('remoteMethodAdded', rebuildSwaggerObject);
+
+  // listening to remoteMethodDisabled event for updating the swaggerObject
+  // when a remote method is disabled to hide that method in the Swagger UI.
+  loopbackApplication.on('remoteMethodDisabled', rebuildSwaggerObject);
+
+  let resourcePath = (opts && opts.resourcePath) || 'swagger.json';
+  if (resourcePath[0] !== '/') resourcePath = '/' + resourcePath;
+
+  swaggerApp.get(resourcePath, function sendSwaggerObject(req, res) {
+    res.status(200).send(swaggerObject);
+  });
 }
 
 function routes(loopbackApplication, options) {
   const loopback = loopbackApplication.loopback;
-  const loopbackMajor =
-    (loopback && loopback.version && loopback.version.split('.')[0]) || 1;
-
-  if (loopbackMajor < 2) {
-    throw new Error(
-      g.f(
-        '{{loopback-component-explorer}} requires ' +
-          '{{loopback}} 2.0 or newer'
-      )
-    );
-  }
 
   options = _defaults({}, options, {
     resourcePath: 'swagger.json',
     apiInfo: loopbackApplication.get('apiInfo') || {},
-    swaggerUI: false,
+    swaggerUI: true,
   });
 
   const router = new loopback.Router();
@@ -81,44 +89,46 @@ function routes(loopbackApplication, options) {
     });
   });
 
+  if (options.swaggerUI) {
+    // Allow specifying a static file roots for swagger files. Any files in
+    // these folders will override those in the swagger-ui distribution.
+    // In this way one could e.g. make changes to index.html without having
+    // to worry about constantly pulling in JS updates.
+    if (options.uiDirs) {
+      if (typeof options.uiDirs === 'string') {
+        router.use(loopback.static(options.uiDirs));
+      } else if (Array.isArray(options.uiDirs)) {
+        options.uiDirs.forEach(function(dir) {
+          router.use(loopback.static(dir));
+        });
+      }
+    }
+
+    // File in node_modules are overridden by a few customizations
+    router.use(loopback.static(STATIC_ROOT));
+
+    // Swagger UI distribution
+    router.use(loopback.static(SWAGGER_UI_ROOT));
+  }
+
   return router;
 }
 
 /**
- * Setup Swagger documentation on the given express app.
+ * Example usage:
  *
- * @param {Application} loopbackApplication The loopback application to
- * document.
- * @param {Application} swaggerApp Swagger application used for hosting
- * swagger documentation.
- * @param {Object} opts Options.
+ * var explorer = require('loopback-component-explorer');
+ * explorer(app, options);
  */
-function mountSwagger(loopbackApplication, swaggerApp, opts) {
-  let swaggerObject = createSwaggerObject(loopbackApplication, opts);
 
-  // listening to modelRemoted event for updating the swaggerObject
-  // with the newly created model to appear in the Swagger UI.
-  loopbackApplication.on('modelRemoted', rebuildSwaggerObject);
-
-  loopbackApplication.on('modelDeleted', rebuildSwaggerObject);
-
-  // listening to started event for updating the swaggerObject
-  // when a call to app.models.[modelName].nestRemoting([modelName])
-  // to appear that method in the Swagger UI.
-  loopbackApplication.on('remoteMethodAdded', rebuildSwaggerObject);
-
-  // listening to remoteMethodDisabled event for updating the swaggerObject
-  // when a remote method is disabled to hide that method in the Swagger UI.
-  loopbackApplication.on('remoteMethodDisabled', rebuildSwaggerObject);
-
-  let resourcePath = (opts && opts.resourcePath) || 'swagger.json';
-  if (resourcePath[0] !== '/') resourcePath = '/' + resourcePath;
-
-  swaggerApp.get(resourcePath, function sendSwaggerObject(req, res) {
-    res.status(200).send(swaggerObject);
-  });
-
-  function rebuildSwaggerObject() {
-    swaggerObject = createSwaggerObject(loopbackApplication, opts);
-  }
+function explorer(loopbackApplication, options) {
+  options = _defaults({}, options, {mountPath: '/explorer'});
+  loopbackApplication.use(
+    options.mountPath,
+    routes(loopbackApplication, options)
+  );
+  loopbackApplication.set('loopback-component-explorer', options);
 }
+
+module.exports = explorer;
+explorer.routes = routes;
--- test/explorer.test.js
+++ test/explorer.test.js
@@ -8,9 +8,32 @@
 const loopback = require('loopback');
 const explorer = require('../');
 const request = require('supertest');
+const assert = require('assert');
+const path = require('path');
 const expect = require('chai').expect;
 
 describe('explorer', function() {
+
+  function configureRestApiAndExplorer(app, explorerBase) {
+    const Product = loopback.PersistedModel.extend('product');
+    Product.attachTo(loopback.memory());
+    app.model(Product);
+
+    explorer(app, {mountPath: explorerBase});
+    app.set('legacyExplorer', false);
+    app.use(app.get('restApiRoot') || '/', loopback.rest());
+  }
+
+  function givenLoopBackAppWithExplorer(explorerBase) {
+    return function(done) {
+      const app = this.app = loopback();
+      app.set('remoting', {cors: false});
+      configureRestApiAndExplorer(app, explorerBase);
+
+      done();
+    };
+  }
+
   describe('with default config', function() {
     beforeEach(givenLoopBackAppWithExplorer());
 
@@ -19,22 +42,29 @@ describe('explorer', function() {
         .to.have.property('mountPath', '/explorer');
     });
 
-    it('should return 404 not found', function(done) {
+    it('should redirect to /explorer/', function(done) {
       request(this.app)
         .get('/explorer')
-        .expect(404)
+        .expect(301)
         .end(done);
     });
 
     it('should serve the explorer at /explorer/', function(done) {
       request(this.app)
         .get('/explorer/')
-        .expect('Content-Type', /json/)
-        .expect(404)
-        .end(done);
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+
+          assert(Boolean(res.text.indexOf('<title>LoopBack API Explorer</title>') !== -1),
+            'text does not contain expected string');
+
+          done();
+        });
     });
 
-    it('should serve correct swagger config', function(done) {
+    it('should serve correct swagger-ui config', function(done) {
       request(this.app)
         .get('/explorer/config.json')
         .expect('Content-Type', /json/)
@@ -53,11 +83,19 @@ describe('explorer', function() {
   describe('when filename is included in url', function() {
     beforeEach(givenLoopBackAppWithExplorer());
 
-    it('should not serve the explorer at /explorer/index.html', function(done) {
+    it('should serve the explorer at /explorer/index.html', function(done) {
       request(this.app)
         .get('/explorer/index.html')
-        .expect(404)
-        .end(done);
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+
+          assert(Boolean(res.text.indexOf('<title>LoopBack API Explorer</title>') !== -1),
+            'text does not contain expected string');
+
+          done();
+        });
     });
 
     it('should serve correct swagger-ui config', function(done) {
@@ -145,6 +183,77 @@ describe('explorer', function() {
     });
   });
 
+  describe('with custom front-end files', function() {
+    let app;
+    beforeEach(function setupExplorerWithUiDirs() {
+      app = loopback();
+      app.set('remoting', {cors: false});
+      explorer(app, {
+        uiDirs: [path.resolve(__dirname, 'fixtures', 'dummy-swagger-ui')],
+      });
+    });
+
+    it('overrides swagger-ui files', function(done) {
+      request(app).get('/explorer/swagger-ui.js')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+
+          // expect the content of `dummy-swagger-ui/swagger-ui.js`
+          expect(res.text).to.contain('/* custom swagger-ui file */');
+
+          done();
+        });
+    });
+
+    it('overrides strongloop overrides', function(done) {
+      request(app).get('/explorer/')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          // expect the content of `dummy-swagger-ui/index.html`
+          expect(res.text).to.contain('custom index.html');
+          done();
+        });
+    });
+  });
+
+  describe('with swaggerUI option', function() {
+    let app;
+    beforeEach(function setupExplorerWithoutUI() {
+      app = loopback();
+      app.set('remoting', {cors: false});
+      explorer(app, {
+        swaggerUI: false,
+      });
+    });
+
+    it('overrides swagger-ui files', function(done) {
+      request(app).get('/explorer/swagger-ui.js')
+        .expect(404, done);
+    });
+
+    it('should serve config.json', function(done) {
+      request(app)
+        .get('/explorer/config.json')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+
+          expect(res.body).to
+            .have.property('url', '/explorer/swagger.json');
+
+          done();
+        });
+    });
+
+    it('should serve swagger.json', function(done) {
+      request(app)
+        .get('/explorer/swagger.json')
+        .expect(200, done);
+    });
+  });
+
   describe('explorer.routes API', function() {
     let app;
     beforeEach(function() {
@@ -164,6 +273,44 @@ describe('explorer', function() {
         .expect('Content-Type', /json/)
         .expect(200)
         .end(done);
+    });
+  });
+
+  describe('when specifying custom static file root directories', function() {
+    let app;
+    beforeEach(function() {
+      app = loopback();
+      app.set('remoting', {cors: false});
+    });
+
+    it('should allow `uiDirs` to be defined as an Array', function(done) {
+      explorer(app, {
+        uiDirs: [path.resolve(__dirname, 'fixtures', 'dummy-swagger-ui')],
+      });
+
+      request(app).get('/explorer/')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          // expect the content of `dummy-swagger-ui/index.html`
+          expect(res.text).to.contain('custom index.html');
+          done();
+        });
+    });
+
+    it('should allow `uiDirs` to be defined as an String', function(done) {
+      explorer(app, {
+        uiDirs: path.resolve(__dirname, 'fixtures', 'dummy-swagger-ui'),
+      });
+
+      request(app).get('/explorer/')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          // expect the content of `dummy-swagger-ui/index.html`
+          expect(res.text).to.contain('custom index.html');
+          done();
+        });
     });
   });
 
@@ -287,7 +434,7 @@ describe('explorer', function() {
         expect(paths).to.contain('/products/findOne');
 
         const Product = app.models.Product;
-        Product.findOne2 = function(cb) { cb(null, 1); };
+        Product.findOne2 = (cb) => cb(null, 1);
         Product.remoteMethod('findOne2', {});
 
         // Request swagger.json again
@@ -304,24 +451,4 @@ describe('explorer', function() {
           });
       });
   });
-
-  function givenLoopBackAppWithExplorer(explorerBase) {
-    return function(done) {
-      const app = this.app = loopback();
-      app.set('remoting', {cors: false});
-      configureRestApiAndExplorer(app, explorerBase);
-
-      done();
-    };
-  }
-
-  function configureRestApiAndExplorer(app, explorerBase) {
-    const Product = loopback.PersistedModel.extend('product');
-    Product.attachTo(loopback.memory());
-    app.model(Product);
-
-    explorer(app, {mountPath: explorerBase});
-    app.set('legacyExplorer', false);
-    app.use(app.get('restApiRoot') || '/', loopback.rest());
-  }
 });
